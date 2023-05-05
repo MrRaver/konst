7eaf57b3f317a3384eb2d5a31a673e8ae27ed226
Tashkinov Viktor <114143145+MrRaver@users.noreply.github.com>
2023-05-05 15:37:29 +0500

    Update filter_test.py

--- tests/filter_test.py
+++ tests/filter_test.py
@@ -11,26 +11,28 @@ class TestFilters(BaseTest):
         fil.filter_additional()
 
 
-     def test_filter_table3(self, driver):  # 2.Фильтрация данных в таблице
+    """ def test_filter_table3(self, driver):  # 2.Фильтрация данных в таблице
         fil = Test_Filters(driver, self.main_url() + self.login_url())
         fil.open()
         fil.filter_table3()
+      """
 
 
-def test_filter_additional_zakr(self, driver):  # 1.Закрепление столбцов и сортировка (представление администратора)
-    fil = Test_Filters(driver, self.main_url() + self.login_url())
-    fil.open()
-    fil.filter_additional_zakr()
+     def test_filter_additional_zakr(self, driver):  # 1.Закрепление столбцов и сортировка (представление администратора)
+         fil = Test_Filters(driver, self.main_url() + self.login_url())
+         fil.open()
+         fil.filter_additional_zakr()
 
 
-def test_filter_additional_visible(self,driver):  # 2.Скрытие столбцов и настройка ширины (представление администратора)
-    fil = Test_Filters(driver, self.main_url() + self.login_url())
-    fil.open()
-    fil.filter_additional_visible()
+     def test_filter_additional_visible(self,driver):  # 2.Скрытие столбцов и настройка ширины (представление администратора)
+         fil = Test_Filters(driver, self.main_url() + self.login_url())
+         fil.open()
+         fil.filter_additional_visible()
 
-
-def test_filter_additional_visible_and_move(self, driver):  # 3.Скрытие столбцов и настройка ширины (представление администратора)
-    fil = Test_Filters(driver, self.main_url() + self.login_url())
-    fil.open()
-    fil.filter_additional_visible_and_move()
+ """
+     def test_filter_additional_visible_and_move(self, driver):  # 3.Скрытие столбцов и настройка ширины (представление администратора)
+         fil = Test_Filters(driver, self.main_url() + self.login_url())
+         fil.open()
+         fil.filter_additional_visible_and_move()
+        """
 
